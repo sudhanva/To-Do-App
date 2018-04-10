24135efb71973d5463505925376a0d103a69256a
sudhanva <sudhanva.sa@gmail.com>
2018-04-10 16:49:59 +0530

    display task

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var app= angular.module('TodoList',[]);
 
 
 app.controller('addTaskController',function($scope){
+	$scope.tasks = [];
+
 	$scope.searchEnter= function(){
-		console.log(event.which || event.KeyCode);
-		if(event.which == 13){
+		//console.log(event.which || event.KeyCode);
+		if(event.which == 13 && $scope.task != '') {
 			$scope.addTask();
 		}
 		$scope.addTask = function(){
-
+			$scope.tasks.push({'taskMessage':$scope.task,'status':false});
+			console.log($scope.tasks);
+			$scope.task ='';
 		};
 	};
 });
