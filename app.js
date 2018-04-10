cf4415535b419645d04dda21692e424428fb4d69
sudhanva <sudhanva.sa@gmail.com>
2018-04-10 18:22:47 +0530

    Local Storage

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app= angular.module('TodoList',[]);
 
 app.controller('addTaskController',function($scope){
 	$scope.tasks = [];
+	var taskData = localStorage['tasklist'];
+	if(taskData !== undefined){
+		$scope.tasks=JSON.parse(taskData);
+	}
 
 	$scope.searchEnter= function(){
 		//console.log(event.which || event.KeyCode);
@@ -13,18 +17,27 @@ app.controller('addTaskController',function($scope){
 			$scope.tasks.push({'taskMessage':$scope.task,'status':false});
 			//console.log($scope.tasks);
 			$scope.task ='';
+			localStorage['tasklist'] = JSON.stringify($scope.tasks);
 		};
 
-		$scope.edit = function(){ 
+		$scope.edit = function(msg){ 
+			for(i=0;i<$scope.tasks.length;i++){
+				if($scope.tasks[i].taskMessage == msg){
+					$scope.tasks[i].taskMessage = event.target.innerText;
+
+				}
+			}
+			localStorage['tasklist'] = JSON.stringify($scope.tasks);
 			event.target.contentEditable= event.target.contentEditable == "false" ? "true" : "false";
+			
 		}
 
 		$scope.enterAgain= function(msg){
-			console.log(msg);
+			//console.log(msg);
 			if(event.which == 13 && msg != '') {
-				$scope.edit();
+				$scope.edit(msg);
 			}
-			console.log("here")
+			//console.log("here")
 		}
 	};
 });
